7d407a07eeceaa8d2cb00998be26491959612f6d
flordeciruela <wendy.reyessan@gmail.com>
2017-12-14 21:25:08 -0500

    get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private blog: string;
+  private location: string;
+  private sumate: string;
+  mostrar = 'fsdf';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get('https://api.github.com/users/Laboratoria')
+    .map((res:any) => {
+      res.sumate = res.email;
+      return res;
+    })
+    .subscribe(
+      (res: IResponse) => {
+        this.blog = res.blog;
+        this.location = res.location;
+        this.sumate = res.sumate;
+      }
+    )
+  }
+
+}
+
+interface IResponse {
+  blog: string;
+  location: string;
+  sumate: string;
 }
